e3dc5bb06326acaf1690970b734b0c853fb13158
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-11-06 20:17:13 -0300

    Curinga Delimitado

---
+++ src/set_map_generics/generic/curinga_delimitados/application/Program.java
@@ -0,0 +1,34 @@
+package set_map_generics.generic.curinga_delimitados.application;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import set_map_generics.generic.curinga_delimitados.entities.Circle;
+import set_map_generics.generic.curinga_delimitados.entities.Rectangle;
+import set_map_generics.generic.curinga_delimitados.entities.Shape;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Shape> myShapes = new ArrayList<>();
+        myShapes.add(new Rectangle(3.0, 2.0));
+        myShapes.add(new Circle(2.0));
+
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
+
+        System.out.println("Total area: " + totalArea(myCircles));
+    }
+
+    public static double totalArea(List<? extends Shape> list) {
+        double sum = 0.0;
+        for (Shape s : list) {
+            sum += s.area();
+        }
+
+        return sum;
+    }
+}
---
+++ src/set_map_generics/generic/curinga_delimitados/entities/Shape.java
@@ -0,0 +1,7 @@
+package set_map_generics.generic.curinga_delimitados.entities;
+
+public interface Shape {
+    double area();
+
+
+}
